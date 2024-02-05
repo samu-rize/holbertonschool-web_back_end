c12e7bb97fd2b2dee584bb549666cbed384e382f
samu-rize <6710@holbertonstudents.com>
2024-02-05 17:44:53 +0100

    Function add documented

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """
-0. Basic annotations - add
-Write a type-annotated function add that takes a float a and a float b as
+this is a type-annotated function add that takes a float a and a float b as
 arguments and returns their sum as a float.
 """
 
 
 def add(a: float, b: float) -> float:
+    """
+    function takes 2 floats
+    then return the sum as a float"""
     return (a + b)
