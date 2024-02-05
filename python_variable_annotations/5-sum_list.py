fc64ed81b068f94ab8ac91b9520014d8fd38f211
samu-rize <6710@holbertonstudents.com>
2024-02-05 19:16:35 +0100

    fix the type of the list

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -4,9 +4,9 @@
 """
 from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     this is a type-annotated function sum_list which takes a list input_list
     of floats as argument and returns their sum as a float.
     """
-    return (sum(input_list))
+    return sum(input_list)
