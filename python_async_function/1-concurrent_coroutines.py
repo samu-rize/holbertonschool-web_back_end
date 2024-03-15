aec4586efcf297537f659da5e89c203518d6e059
samu-rize <6710@holbertonstudents.com>
2024-03-15 18:43:31 +0100

    try to fix the error

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''
+"""
 Import wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes
 in 2 int arguments (in this order): n and max_delay.
@@ -8,25 +8,22 @@ You will spawn wait_random n times with the specified max_delay.
 wait_n should return the list of all the delays (float values).
 The list of the delays should be in ascending order without
 using sort() because of concurrency.
-'''
+"""
 
 
 import asyncio
-import random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """ Waits for random delay until max_delay, returns list of actual delays """
-    spawn_list = []
-    delay_list = []
-    for i in range(n):
-        delayed_task = asyncio.create_task(wait_random(max_delay))
-        delayed_task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(delayed_task)
+    """Waits for random delays up to max_delay, returns delays in ascending order"""
+    spawn_list, delay_list = [], []
+    for _ in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        task.add_done_callback(lambda x: delay_list.append(x.result()))
+        spawn_list.append(task)
 
-    for spawn in spawn_list:
-        await spawn
+    for task in spawn_list:
+        await task
 
-    return delay_list
+    return sorted(delay_list)
