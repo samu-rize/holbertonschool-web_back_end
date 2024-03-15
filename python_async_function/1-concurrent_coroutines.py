6304a36fa61765add2ba21d46cd06bbfaf8c92c2
samu-rize <6710@holbertonstudents.com>
2024-03-15 18:55:55 +0100

    try to fix the error

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python3
 """
-Import wait_random from the previous python file that you’ve written
-and write an async routine called wait_n that takes
-in 2 int arguments (in this order): n and max_delay.
-You will spawn wait_random n times with the specified max_delay.
-
-wait_n should return the list of all the delays (float values).
-The list of the delays should be in ascending order without
-using sort() because of concurrency.
+execute multiple coroutines at the same time with async
 """
-
-
 import asyncio
 from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """Waits for random delays up to max_delay, returns delays in ascending order"""
-    spawn_list, delay_list = [], []
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        task.add_done_callback(lambda x: delay_list.append(x.result()))
-        spawn_list.append(task)
 
-    for task in spawn_list:
-        await task
-
-    return sorted(delay_list)
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """execute multiple coroutines at the same time"""
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
