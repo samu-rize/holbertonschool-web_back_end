c89b42277ba61af7247a3a28554cb6dd76d4499b
samu-rize <6710@holbertonstudents.com>
2024-03-16 14:40:50 +0100

    fix pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -12,6 +12,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """execute multiple coroutines at the same time"""
     delays = []
-    for _ in range (n):
+    for _ in range(n):
         delays.append(await wait_random(max_delay))
     return sorted(delays)
